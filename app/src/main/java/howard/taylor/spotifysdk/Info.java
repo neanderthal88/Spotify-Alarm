91dc998090948143b0efcea9d278e882982b5d9f
Taylor R. Howard <taylorhoward10@gmail.com>
2015-09-26 23:35:25 -0500

    Exploring spotify api, TODO: add alarm functionality

---
+++ app/src/main/java/howard/taylor/spotifysdk/Info.java
@@ -0,0 +1,9 @@
+package howard.taylor.spotifysdk;
+
+/**
+ * Created by taylo_000 on 9/26/2015.
+ */
+public class Info {
+    String ClientID = "26abbb67eb634f94924e99be082bcd79";
+    String REDIRECT_URI = "alarm://callback";
+}
---
+++ app/src/main/java/howard/taylor/spotifysdk/MainActivity.java
@@ -0,0 +1,275 @@
+package howard.taylor.spotifysdk;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.AsyncTask;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Config;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerNotificationCallback;
+import com.spotify.sdk.android.player.PlayerState;
+import com.spotify.sdk.android.player.Spotify;
+import com.squareup.okhttp.Response;
+
+import org.w3c.dom.Text;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Album;
+import kaaes.spotify.webapi.android.models.Pager;
+import kaaes.spotify.webapi.android.models.Playlist;
+import kaaes.spotify.webapi.android.models.PlaylistSimple;
+import kaaes.spotify.webapi.android.models.Track;
+import kaaes.spotify.webapi.android.models.UserPrivate;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+
+public class MainActivity extends AppCompatActivity implements ConnectionStateCallback, PlayerNotificationCallback {
+    private static final String CLIENT_ID = new Info().ClientID;
+    private static final String REDIRECT_URI = new Info().REDIRECT_URI;
+    private static final int REQUEST_CODE = 0;
+    private Player mPlayer;
+    BroadcastReceiver _broadcastReceiver;
+    private final SimpleDateFormat _sdfWatchTime = new SimpleDateFormat("hh:mm a");
+    private TextView _tvTime;
+    private SpotifyApi api;
+    private SpotifyService spotify;
+    private Context context;
+    private ProgressDialog progDailog;
+
+    ArrayList<String> listOfPlaylists = new ArrayList<>();
+    List<PlaylistSimple> playlists = new ArrayList<>();
+    private String userID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        context = getApplicationContext();
+        _tvTime = (TextView) findViewById(R.id.time);
+        _tvTime.setText(_sdfWatchTime.format(new Date()));
+
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
+                AuthenticationResponse.Type.TOKEN,
+                REDIRECT_URI);
+
+        builder.setScopes(new String[]{"playlist-read-private", "user-read-private", "streaming", "user-read-email"});
+        AuthenticationRequest request = builder.build();
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+
+        api = new SpotifyApi();
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        _tvTime = (TextView) findViewById(R.id.time);
+        _tvTime.setText(_sdfWatchTime.format(new Date()));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                Log.e("MainActivity", "TOKEN: " + response.getAccessToken());
+                api.setAccessToken(response.getAccessToken());
+                spotify = api.getService();
+
+                Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
+                mPlayer = Spotify.getPlayer(playerConfig, this, new Player.InitializationObserver() {
+                    @Override
+                    public void onInitialized(Player player) {
+                        mPlayer.addConnectionStateCallback(MainActivity.this);
+                        mPlayer.addPlayerNotificationCallback(MainActivity.this);
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
+
+                    }
+                });
+
+            }
+        }
+    }
+
+    public void stuffButtonClick(View view) {
+        new getUserIDSync().execute();
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        _broadcastReceiver = new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context ctx, Intent intent) {
+                if (intent.getAction().compareTo(Intent.ACTION_TIME_TICK) == 0)
+                    _tvTime.setText(_sdfWatchTime.format(new Date()));
+            }
+        };
+
+        registerReceiver(_broadcastReceiver, new IntentFilter(Intent.ACTION_TIME_TICK));
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (_broadcastReceiver != null)
+            unregisterReceiver(_broadcastReceiver);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onLoggedIn() {
+        Log.d("MainActivity", "User logged in");
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Log.d("MainActivity", "User logged out");
+    }
+
+    @Override
+    public void onLoginFailed(Throwable error) {
+        Log.d("MainActivity", "Login failed");
+    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d("MainActivity", "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("MainActivity", "Received connection message: " + message);
+    }
+
+    @Override
+    public void onPlaybackEvent(EventType eventType, PlayerState playerState) {
+        Log.d("MainActivity", "Playback event received: " + eventType.name());
+        switch (eventType) {
+            // Handle event type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onPlaybackError(ErrorType errorType, String errorDetails) {
+        Log.d("MainActivity", "Playback error received: " + errorType.name());
+        switch (errorType) {
+            // Handle error type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        // VERY IMPORTANT! This must always be called or else you will leak resources
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
+    }
+
+
+
+    class getUserIDSync extends AsyncTask<Void, Void, Integer> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progDailog = new ProgressDialog(MainActivity.this);
+            progDailog.setMessage("Loading...");
+            progDailog.setIndeterminate(false);
+            progDailog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            progDailog.setCancelable(true);
+            progDailog.show();
+        }
+        @Override
+        protected Integer doInBackground(Void... params) {
+            userID = spotify.getMe().id;
+            return 1;
+        }
+        @Override
+        protected void onPostExecute(Integer i) {
+            new getPlayListSync().execute();
+        }
+
+    }
+
+    class getPlayListSync extends AsyncTask<Void, Void, Integer> {
+
+        @Override
+        protected Integer doInBackground(Void... params) {
+            playlists = spotify.getPlaylists(userID).items;
+            return 1;
+        }
+
+        protected void onPostExecute(Integer integer) {
+            for (PlaylistSimple list : playlists) {
+                listOfPlaylists.add(list.name);
+                Log.d("Play", list.name);
+            }
+            Intent intent = new Intent(context, ListOfPlaylists.class);
+            Bundle b = new Bundle();
+            b.putStringArrayList("playlist", listOfPlaylists);
+            intent.putExtras(b);
+            progDailog.dismiss();
+            startActivity(intent);
+        }
+
+
+    }
+}
