a9db86b44a5a64faa3644514d73968a949984f92
Taylor R. Howard <taylorhoward10@gmail.com>
2015-09-26 23:38:40 -0500

    fixed files

--- app/src/main/java/howard/taylor/spotifysdk/Info.java
+++
@@ -1,9 +0,0 @@
-package howard.taylor.spotifysdk;
-
-/**
- * Created by taylo_000 on 9/26/2015.
- */
-public class Info {
-    String ClientID = "26abbb67eb634f94924e99be082bcd79";
-    String REDIRECT_URI = "alarm://callback";
-}
